bee2aa5e2b3f0c8d613fadee8f760aff6965a8a1
Raul Urechiatu <raulurechiatu@gmail.com>
2020-11-19 15:21:45 +0200

    added the main class

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static String exB(List<Device> devices) {
+
+	}
+	
+	public static List<Device> exC(List<Device> devices) {
+
+	}
+	
+	public static List<Device> exD(List<Device> devices) {
+
+	}
+	
+	public static Map<String, List<Device>> exE(List<Device> devices) {
+
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(exB(devices));
+		
+		System.out.println(exC(devices));
+		
+		System.out.println(exD(devices));
+		
+		System.out.println(exE(devices));
+	}
+}
